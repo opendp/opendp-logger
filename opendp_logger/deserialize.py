e0281658323bd308816582dcb6b3f71ea48a7b74
Shoeboxam <shoeboxam@gmail.com>
2022-11-08 23:15:30 -0500

    final pass

--- opendp_logger/deserialize.py
+++ opendp_logger/deserialize.py
@@ -1,44 +1,36 @@
-import opendp.transformations as transformations
-import opendp.measurements as measurements
-import opendp.combinators as combinators
+import opendp
 
 import json
 import builtins
-from opendp_logger.constants import PT_TYPE_PREFIX, OPENDP_VERSION
+
+import pkg_resources
+
+try:
+    OPENDP_VERSION = pkg_resources.get_distribution("opendp").version
+except pkg_resources.DistributionNotFound:
+    OPENDP_VERSION = "0.0.0+development"
 
 __all__ = ["make_load_json", "make_load_ast"]
 
 
 def decode_ast(obj):
     if isinstance(obj, dict):
+        if obj.get("_type") == "type":
+            return getattr(builtins, obj["name"])
+
         if obj.get("_type") == "tuple":
             return tuple(decode_ast(i) for i in obj["_items"])
 
         if obj.get("_type") == "constructor":
-            obj = {
-                **obj,
-                "args": decode_ast(obj["args"]),
-                "kwargs": decode_ast(obj["kwargs"]),
-            }
-            if obj["module"] == "transformations":
-                module = transformations
-            elif obj["module"] == "measurements":
-                module = measurements
-            elif obj["module"] == "combinators":
-                module = combinators
-            else:
-                raise ValueError(f"Unrecognized module {obj['module']}.")
+            module = getattr(opendp, obj["module"])
+            constructor = getattr(module, obj["func"])
 
-            return getattr(module, obj["func"])(*obj["args"], **obj["kwargs"])
+            return constructor(*decode_ast(obj["args"]), **decode_ast(obj["kwargs"]))
 
-        else:
-            return {k: decode_ast(v) for k, v in obj.items()}
+        return {k: decode_ast(v) for k, v in obj.items()}
 
     if isinstance(obj, list):
-        return list(decode_ast(i) for i in obj)
-
-    if isinstance(obj, str) and obj.startswith(PT_TYPE_PREFIX):
-        return getattr(builtins, obj[len(PT_TYPE_PREFIX) :])
+        return [decode_ast(i) for i in obj]
 
     return obj
 
--- opendp_logger/serialize.py
+++ opendp_logger/serialize.py
@@ -1,27 +1,26 @@
 import json
 import opendp as opendp
+from functools import wraps
 
-from opendp_logger.constants import PT_TYPE_PREFIX, OPENDP_VERSION
+from opendp_logger.deserialize import OPENDP_VERSION
 
 import importlib
 
 __all__ = ["enable_logging"]
 
 
-def wrap_constructor(f_str, f, module_name):
+def wrap_constructor(f, module_name):
+    @wraps(f)
     def wrapped(*args, **kwargs):
         chain = f(*args, **kwargs)
         chain.log = {
             "_type": "constructor",
-            "func": f_str,
+            "func": f.__name__,
             "module": module_name,
             "args": args,
             "kwargs": kwargs,
         }
         return chain
-
-    wrapped.__annotations__ = f.__annotations__
-    wrapped.__doc__ = f.__doc__
 
     return wrapped
 
@@ -40,7 +39,7 @@ def to_ast(item):
     if isinstance(item, dict):
         return {key: to_ast(value) for key, value in item.items()}
     if isinstance(item, type):
-        return PT_TYPE_PREFIX + item.__name__
+        return {"_type": "type", "name": item.__name__}
     if isinstance(item, opendp.typing.RuntimeType):
         return str(item)
     return item
@@ -50,20 +49,12 @@ def to_json(chain):
     return json.dumps({"version": OPENDP_VERSION, "ast": chain.to_ast()})
 
 
-enabled = False
-
-
 def enable_logging():
-
-    global enabled
-    if enabled:
-        return
-
     for name in ["transformations", "measurements", "combinators"]:
         module = importlib.import_module(f"opendp.{name}")
         for f in dir(module):
             if f.startswith("make_"):
-                module.__dict__[f] = wrap_constructor(f, getattr(module, f), name)
+                module.__dict__[f] = wrap_constructor(getattr(module, f), name)
 
     opendp.Transformation.to_ast = to_ast
     opendp.Measurement.to_ast = to_ast
@@ -71,4 +62,5 @@ def enable_logging():
     opendp.Transformation.to_json = to_json
     opendp.Measurement.to_json = to_json
 
-    enabled = True
+    # only run once
+    enable_logging.__code__ = (lambda: None).__code__
